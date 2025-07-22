394820524abbf7b2dacfa88bdf0aa8dceb7f483c
surjendu <surjendu65@gmail.com>
2025-07-22 21:56:14 +0530

    Added Add method

--- hello_world/src/java8/Test.java
+++ hello_world/src/java8/Test.java
@@ -3,6 +3,11 @@ package java8;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println(add(2,34));
+    }
+
+    public static int add(int a,int b){
+        return a+b;
     }
 
 }
