ad2ee0a65c000469803f1bc8c4cb020b9d6e39c2
surjendu <surjendu65@gmail.com>
2025-07-23 08:10:37 +0530

    cheking1

---
+++ hello_world/src/java8/Check.java
@@ -0,0 +1,12 @@
+package java8;
+
+public class Check {
+    public static void main(String[] args) {
+        int num = 20;
+        if (num % 2 == 0){
+            System.out.println(num +" is even");
+        }else {
+            System.out.println(num+ " is odd ");
+        }
+    }
+}
